042a99fdfce6a9fd44f0b7381e2365eb190d4df9
Nami <94586342+namijeong@users.noreply.github.com>
2022-09-28 18:36:39 +0900

    Update 3 weibull plot.R

--- application/3 weibull plot.R
+++ application/3 weibull plot.R
@@ -1,17 +1,8 @@
 source("C:/nnn/Rm16.R")
-#data = rweibull(n, shape=shape0, scale=scale0)
+#
 data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
 library(weibullness)
 #data = c(15.1 ,12.2 ,17.3, 14.3 , 7.9 ,18.2 ,24.6 ,13.5, 10.0, 30.5)
-#hist(data)
-weibull.mle(data, threshold=0)
-weibull.wp(data)
-weibull.e(data)
-weibull.seki(data)
-weibull.wmed(data)
-weibull.med1(data)
-weibull.med2(data)
-weibull.med3(data)
 #==============================================================================
 idx.sort = order( data )
 data.sort = data[idx.sort]
@@ -23,13 +14,13 @@ Sweibull <- function(x, shape,scale) {
 
 par( mfrow=c(1,3), mar=c(5,5,3,1), omi=c(0,0,0,0), cex=0.5,mex=0.5 )
 #==============================================================================
-xlim = range( log(data) );  
-ylim = range( log(-log(Fall) ) )
-plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16, xlim=xlim, ylim=ylim, 
+xlim = c(9,14)
+#ylim = range( log(-log(Fall) ) )
+plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16, xlim=xlim,
        xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1.5, cex =1.5  )
 
 xxx = seq(min(X),max(X), l=100)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(data)$shape,scale=weibull.mle(data)$scale))), lty=1, col ="dark gray" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(data,0)$shape,scale=weibull.mle(data,0)$scale))), lty=1, col ="dark gray" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.wp(data)$shape,scale=weibull.wp(data)$scale))), lty=2, col ="Dim Gray" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(data)$shape,scale=weibull.med1(data)$scale))), lty=3, col ="Pale violet red2" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(data)$shape,scale=weibull.med2(data)$scale))), lty=4, col ="dark Orchid2" ,lwd = 2)
@@ -38,17 +29,40 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(data)$shape,scale=wei
 legend (11.8, -2, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =1.5)
 #outlier==============================================================================
 X=sort(data)
-X[20]=X[20]*100
+X[20]=X[20]*5
+idx.sort = order( X )
+data.sort = X[idx.sort]
+Fall     = ppoints( X )
 
-
-plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16, xlim=xlim, ylim=ylim, 
+plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16,xlim=xlim,  
        xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1.5, cex =1.5  )
 
 xxx = seq(min(X),max(X), l=100)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(data)$shape,scale=weibull.mle(data)$scale))), lty=1, col ="dark gray" ,lwd = 2)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.wp(data)$shape,scale=weibull.wp(data)$scale))), lty=2, col ="Dim Gray" ,lwd = 2)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(data)$shape,scale=weibull.med1(data)$scale))), lty=3, col ="Pale violet red2" ,lwd = 2)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(data)$shape,scale=weibull.med2(data)$scale))), lty=4, col ="dark Orchid2" ,lwd = 2)
-lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(data)$shape,scale=weibull.med3(data)$scale))), lty=5, col ="slate blue" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(X,0)$shape,scale=weibull.mle(X,0)$scale))), lty=1, col ="dark gray" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.wp(X)$shape,scale=weibull.wp(X)$scale))), lty=2, col ="Dim Gray" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(X)$shape,scale=weibull.med1(X)$scale))), lty=3, col ="Pale violet red2" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(X)$shape,scale=weibull.med2(X)$scale))), lty=4, col ="dark Orchid2" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(X)$shape,scale=weibull.med3(X)$scale))), lty=5, col ="slate blue" ,lwd = 2)
 
 legend (11.8, -2, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =1.5)
+#inlier==============================================================================
+data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
+X=sort(data)
+X[1]=X[1]*1/5
+idx.sort = order( X )
+data.sort = X[idx.sort]
+Fall     = ppoints( X )
+
+plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16,xlim=xlim,  
+       xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1.5, cex =1.5  )
+
+xxx = seq(min(X),max(X), l=100)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(X,0)$shape,scale=weibull.mle(X,0)$scale))), lty=1, col ="dark gray" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.wp(X)$shape,scale=weibull.wp(X)$scale))), lty=2, col ="Dim Gray" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(X)$shape,scale=weibull.med1(X)$scale))), lty=3, col ="Pale violet red2" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(X)$shape,scale=weibull.med2(X)$scale))), lty=4, col ="dark Orchid2" ,lwd = 2)
+lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(X)$shape,scale=weibull.med3(X)$scale))), lty=5, col ="slate blue" ,lwd = 2)
+
+legend (11.8, -2, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =1.5)
+
+
