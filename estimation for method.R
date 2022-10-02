929826743a9f6305989e5721b433d350a5376303
Nami <94586342+namijeong@users.noreply.github.com>
2022-10-02 18:36:36 +0900

    Update estimation for method.R

--- estimation for method.R
+++ estimation for method.R
@@ -1,7 +1,7 @@
 
 ##source("https://raw.githubusercontent.com/AppliedStat/SCD/master/SCD.R")
 ## source("Rm16.R")
-source("C:/nnn/Rm16.R")
+source("https://raw.githubusercontent.com/namijeong/tar/main/Rm16.R")
 #------------------------------
 library(weibullness)
 
