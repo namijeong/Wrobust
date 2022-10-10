164cdd246c5fc5910b52815330c0338e8536207c
Nami <94586342+namijeong@users.noreply.github.com>
2022-10-11 04:25:42 +0900

    Update shape,scale delta값.R

--- shape,scale delta값.R
+++ shape,scale delta값.R
@@ -1,4 +1,4 @@
-source("C:/nnn/Rm16.R")
+source("https://raw.githubusercontent.com/namijeong/tar/main/Rm16.R")
 
 # DATA 
 ############################################################################################
@@ -83,7 +83,7 @@ lines(DELTA, SHAPE.med2, col="navy blue", type="l", lwd=2,lty=3)
 lines(DELTA, SHAPE.med3, col="slate blue", type="l", lwd=2,lty=4)
 
 legend (1e-05, 4.3,lty=c(1,2,3,1,3,1,3,4) ,col=c("red","Orange red","pale violet red","Lime green", "Forest green","dark blue","navy blue","slate blue"),
-        legend=c("MLE", "WP","seki","e","wmed","med1","med2","med3"), bty="n" ,lwd = 2, cex =2)
+        legend=c("MLE", "WP","seki","e","wmed","med1","med2","med3"), bty="n" ,lwd = 1, cex =1)
 
 plot(NA, NA, xlab=expression(delta),ylab="SCALE" ,log="x", type="n", xlim=range(DELTA), ylim=ylim1 )
 abline( h = scale0, col="black", lwd=1)
@@ -99,4 +99,4 @@ lines(DELTA, SCALE.med2, col="navy blue", type="l", lwd=2,lty=3)
 lines(DELTA, SCALE.med3, col="slate blue", type="l", lwd=2,lty=4)
   
 legend (1e-05, 0.85, lty=c(1,2,3,1,3,1,3,4) ,col=c("red","Orange red","pale violet red","Lime green", "Forest green","dark blue","navy blue","slate blue"),
-        legend=c("MLE", "WP","seki","e","wmed","med1","med2","med3"), bty="n"  ,lwd = 2, cex =2)
+        legend=c("MLE", "WP","seki","e","wmed","med1","med2","med3"), bty="n"  ,lwd = 1, cex =1)
