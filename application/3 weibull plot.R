feb2089ad0f3fbd050c5fc7d4c2e8516718d06f2
Nami <94586342+namijeong@users.noreply.github.com>
2022-12-05 12:48:44 +0900

    Update 3 weibull plot.R

--- application/3 weibull plot.R
+++ application/3 weibull plot.R
@@ -19,7 +19,7 @@ xlim = c(9,14)
 #xlim = c(0,5)
 #ylim = range( log(-log(Fall) ) )
 plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16, xlim=xlim,
-       xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1, cex =1, sub="(a)" )
+       xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1, cex =0.8, sub="(a)" )
 
 xxx = seq(min(X),max(X), l=100)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(data,0)$shape,scale=weibull.mle(data,0)$scale))), lty=1, col ="red" ,lwd = 2)
@@ -28,7 +28,7 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(data)$shape,scale=wei
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(data)$shape,scale=weibull.med2(data)$scale))), lty=3, col ="navy blue" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(data)$shape,scale=weibull.med3(data)$scale))), lty=4, col ="slate blue" ,lwd = 2)
 
-legend (12, -2.5, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =1)
+legend (12, -2.5, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =0.8)
 #legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1.5)
 #outlier==============================================================================
 data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
@@ -71,7 +71,7 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(X)$shape,scale=weibul
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(X)$shape,scale=weibull.med2(X)$scale))), lty=3, col ="navy blue" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(X)$shape,scale=weibull.med3(X)$scale))), lty=4, col ="slate blue" ,lwd = 2)
 
-legend (12, -2.5, lty=c(1,2,3,4,5) ,col=c("dark gray","Dim Gray","Pale violet red2","dark Orchid2","slate blue"),legend=c("MLE", "WP","MED1","MED2","MED3"), bty="n",lwd = 2, cex =1)
+legend (12, -2.5, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
 #legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1.5)
 
 
