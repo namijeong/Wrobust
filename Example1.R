8726a22830ef5c8da47b3f65bac7210b79532a08
Nami <94586342+namijeong@users.noreply.github.com>
2022-12-09 13:30:03 +0900

    Update Example1.R

--- Example1.R
+++ Example1.R
@@ -1,7 +1,7 @@
 
 ##source("https://raw.githubusercontent.com/AppliedStat/SCD/master/SCD.R")
 ## source("Rm16.R")
-source(https://raw.githubusercontent.com/namijeong/Wrobust/main/Rm16.R")
+source("https://raw.githubusercontent.com/namijeong/Wrobust/main/Rm16.R")
 #------------------------------
 
 library(weibullness)
