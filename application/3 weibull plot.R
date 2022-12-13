4ad1523fffe91795a4b7a073f22fcf84784d068b
Nami <94586342+namijeong@users.noreply.github.com>
2022-12-13 20:47:07 +0900

    Update 3 weibull plot.R

--- application/3 weibull plot.R
+++ application/3 weibull plot.R
@@ -1,4 +1,6 @@
-source("https://raw.githubusercontent.com/namijeong/tar/main/Rm16.R")
+rm(list=ls())
+source("https://raw.githubusercontent.com/namijeong/Wrobust/main/Rm16.R")
+
 #
 data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
 library(weibullness)
@@ -12,14 +14,15 @@ X=sort(data)
 Sweibull <- function(x, shape,scale) {
   (1-pweibull(x,shape=shape,scale=scale))}
 
-pdf(file="Fig4.pdf", width=14.5, height=6.6, paper = "special", encoding = "TeXtext.enc")
-par( mfrow=c(1,3), mar=c(6.5,4.8,3,1), omi=c(0,0,0,0), cex=1 ,mex=0.6 )
+png(file="wp2.png", width=1540, height=700)
+
+par( mfrow=c(1,3), mar=c(6.5,4.8,3,1), omi=c(0,0,0,0), cex=1.8 ,mex=0.75 )
 #==============================================================================
 xlim = c(9,14)
 #xlim = c(0,5)
 #ylim = range( log(-log(Fall) ) )
 plot(  log(data.sort),  log(-log(1-Fall)), main = "Weibull Probability", pch=16, xlim=xlim,
-       xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1, cex =0.8, sub="(a)" )
+       xlab="logt", ylab="log{-log(1-F(t))}",lwd = 1, cex =1.0, sub="(a)" )
 
 xxx = seq(min(X),max(X), l=100)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.mle(data,0)$shape,scale=weibull.mle(data,0)$scale))), lty=1, col ="red" ,lwd = 2)
@@ -28,8 +31,8 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med1(data)$shape,scale=wei
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(data)$shape,scale=weibull.med2(data)$scale))), lty=3, col ="navy blue" ,lwd = 2)
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(data)$shape,scale=weibull.med3(data)$scale))), lty=4, col ="slate blue" ,lwd = 2)
 
-legend (12, -2.5, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =0.8)
-#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1.5)
+legend (12, -2.5, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
+#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
 #outlier==============================================================================
 data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
 #data = c(15.1 ,12.2 ,17.3, 14.3 , 7.9 ,18.2 ,24.6 ,13.5, 10.0, 30.5)
@@ -51,7 +54,7 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(X)$shape,scale=weibul
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(X)$shape,scale=weibull.med3(X)$scale))), lty=4, col ="slate blue" ,lwd = 2)
 
 legend (12, -2.5,lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
-#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1.5)
+#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
 #inlier==============================================================================
 data = 1000* c(100,  90,  59, 117, 177,  98, 125, 118,  99, 132, 97, 87, 126, 107, 66, 186,  158,  80,  69, 109)
 #data = c(15.1 ,12.2 ,17.3, 14.3 , 7.9 ,18.2 ,24.6 ,13.5, 10.0, 30.5)
@@ -72,6 +75,6 @@ lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med2(X)$shape,scale=weibul
 lines(log(xxx), log(-log(Sweibull( xxx, shape=weibull.med3(X)$shape,scale=weibull.med3(X)$scale))), lty=4, col ="slate blue" ,lwd = 2)
 
 legend (12, -2.5, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
-#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1.5)
+#legend (0, 1.1, lty=c(1,2,1,3,4) ,col=c("red","orange red","dark blue","navy blue","slate blue"),legend=c("MLE", "WP","med1","med2","med3"), bty="n",lwd = 2, cex =1)
 
-
+dev.off()
